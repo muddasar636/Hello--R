270d0f236ea58ac647bb423b0c6f2ae91fb6cea7
muddasar636 <muddasarsiddiqui27@gmail.com>
2021-03-22 13:52:30 -0700

    few lines added

--- Testing.R
+++ Testing.R
@@ -1,2 +1,8 @@
 # This is from Github
-# print("Hello from Github")
+ print("Hello from Github")
+name = ("muddasar")
+version = "R studio - In Writing"
+
+print (name)
+print("These lines has been written in R studio")
+
